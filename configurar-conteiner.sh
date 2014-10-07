0a758f01c90a21c202f74e8eb3c73a1de382b7ce
Paulo Jerônimo <pj@paulojeronimo.info>
2014-10-07 03:08:29 -0300

    Published gh-pages to GitHub pages.

---
+++ configurar-conteiner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Author: Paulo Jerônimo (@paulojeronimo, paulojeronimo@gmail.com)
+
+unset ok
+while [[ ! $ok =~ ^[Ss]$ ]]
+do
+  read -p 'Usuário no GitHub.............: ' GHUSER
+  read -p 'Fork do projeto sislegis-site.: ' GHFORK
+  read -p 'Nome..........................: ' NOME
+  read -p 'Email.........................: ' EMAIL
+  GHFORK=${GHFORK:-http://github.com/$GHUSER/sislegis-site}
+  echo -e "\nGHUSER=$GHUSER\nGHFORK=$GHFORK\nNOME=$NOME\nEMAIL=$EMAIL\n"
+  read -p "Confirma? (s/N): " ok
+done
+
+useradd -G rvm $GHUSER -s /bin/bash
+su - $GHUSER <<EOF
+git config --global user.name "$NOME"
+git config --global user.email "$EMAIL"
+git config --global push.default simple
+git clone $GHFORK
+cd sislegis-site
+rvm use 2.1@sislegis-site --create --ruby-version
+bundle install
+EOF
